b4027a2ef0b947e0da47b7299f275fcbd6cb9ed6
catur <catur@fisip.net>
2024-07-06 22:16:06 +0700

    add: metadata

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -5,8 +5,13 @@
     <meta charset="utf-8">
     <title>{{str_replace('-', ' ', config('app.name'))}} | {{$title}}</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
+    <meta content="Safwana Marine International adalah perusahaan dengan kualitas tertinggi yang memperkerjakan anggota kru Indonesia yang sangat kompeten untuk bekerja di perusahaan pelayaran dengan semua jenis kapal & ukuran kapal. Kami mempunyai prosedur yang sangat ketat namun adil dalam proses perekrutan anggota kru. Kami juga menggunakan metode perincian dan seleksi modern untuk memastikan bahwa anggota kru kami memiliki kualitas dan kepribadi profesional yang diperlukan klien kami. Kami juga memastikan bahwa anggota kru siap untuk memenuhi standard yang anda butuhkan dan juga sangat berpengalaman untuk bekerja di atas kapal" name="description">
+    <meta content="kru kapal indonesia, kapal, agency kapal, perusahaan kapal, manner agency" name="keywords">
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta name="googlebot-news" content="index, follow">
+    <meta name="developer" content="caturilham05.github.io/portfolio">
+    <meta name="Author" content="{{url('/')}}">
 
     <!-- Favicon -->
     <link href="{{asset('assets/img/favicon.ico')}}" rel="icon">
