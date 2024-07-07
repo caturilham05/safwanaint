802a45479a057731eab6d0783d369dab030738c3
catur <catur@fisip.net>
2024-07-07 20:11:23 +0700

    fix: empty description imageslider

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,22 +4,26 @@
 <div class="container-fluid p-0 mb-5">
     <div class="owl-carousel header-carousel position-relative">
         @foreach ($imageslider as $slider)
-            @php
-            preg_match('/<p>(.*?)<\/p>/is', $slider->description, $match);
-            $description = $match[1];
-            @endphp
+            @if (!empty($slider->description))
+                @php
+                preg_match('/<p>(.*?)<\/p>/is', $slider->description, $match);
+                $description = $match[1];
+                @endphp
+            @endif
             <div class="owl-carousel-item position-relative">
                 <img class="img-fluid" src="{{asset('/storage/'.$slider->media[0]->id.'/'.$slider->media[0]->file_name)}}" alt="">
                 <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(0, 0, 0, .4);">
-                    <div class="container">
-                        <div class="row justify-content-start">
-                            <div class="col-10 col-lg-8">
-                                <h5 class="text-white text-uppercase mb-3 animated slideInDown">{{$slider->intro}}</h5>
-                                <h1 class="display-3 text-white animated slideInDown mb-4">{{$slider->title}}</h1>
-                                <p class="fs-5 fw-medium text-white mb-4 pb-2">{!! $description !!}</p>
+                    @if (!empty($description))                    
+                        <div class="container">
+                            <div class="row justify-content-start">
+                                <div class="col-10 col-lg-8">
+                                    <h5 class="text-white text-uppercase mb-3 animated slideInDown">{{$slider->intro}}</h5>
+                                    <h1 class="display-3 text-white animated slideInDown mb-4">{{$slider->title}}</h1>
+                                    <p class="fs-5 fw-medium text-white mb-4 pb-2">{!! $description ?? '' !!}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                 </div>
             </div>
         @endforeach
