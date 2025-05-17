1cc8a4c1a8f6e59ede07e9de1da509400849a765
catur <catur@fisip.net>
2025-05-17 21:19:10 +0700

    fix: kondition ==

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 @php
 $contacts = $contacts->filter(function($item){
-    return $item->is_show === 1;
-})
+    return $item->is_show == 1;
+});
 @endphp
 <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
     <div class="container py-5">
